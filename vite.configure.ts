8faec77a41f28fabebd78600588f450b958937b4
Nico055O <aldavisthedog55@gmail.com>
2025-04-23 12:50:00 -0700

    Tailwind Lesson Testing

---
+++ vite.configure.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+})
+
+// This is a Vite configuration file that sets up the Tailwind CSS plugin for Vite. 
+
+// Add @import "tailwindcss"; to each file. 
