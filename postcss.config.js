91b7300a1438f273a0a35d659dde80fb3cf8ab34
Nico055O <aldavisthedog55@gmail.com>
2025-04-24 14:06:23 -0700

    Tailwindfvgawe

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  }
+//
+// This file is used to configure PostCSS, a tool for transforming CSS with JavaScript.
+// It specifies that the PostCSS plugins to be used are Tailwind CSS and Autoprefixer.
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
+  
